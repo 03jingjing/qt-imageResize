79f66e867a302a8d8c0568fa1502b62638b878bb
03jingjing <1281342986@qq.com>
2025-09-17 13:44:39 +0800

    Delete build directory

--- build/Desktop_Qt_6_9_1_MSVC2022_64bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_imageResize_FILE C:/Dev/Qproject/imageResize/build/Desktop_Qt_6_9_1_MSVC2022_64bit-Debug/imageResize.exe)
-set(__QT_DEPLOY_TARGET_imageResize_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_imageResize_RUNTIME_DLLS C:/Qt/6.9.1/msvc2022_64/bin/Qt6Widgetsd.dll;C:/Qt/6.9.1/msvc2022_64/bin/Qt6Guid.dll;C:/Qt/6.9.1/msvc2022_64/bin/Qt6Cored.dll)
--- build/Desktop_Qt_6_9_1_MSVC2022_64bit-Release/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_imageResize_FILE C:/Dev/Qproject/imageResize/build/Desktop_Qt_6_9_1_MSVC2022_64bit-Release/imageResize.exe)
-set(__QT_DEPLOY_TARGET_imageResize_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_imageResize_RUNTIME_DLLS C:/Qt/6.9.1/msvc2022_64/bin/Qt6Widgets.dll;C:/Qt/6.9.1/msvc2022_64/bin/Qt6Gui.dll;C:/Qt/6.9.1/msvc2022_64/bin/Qt6Core.dll)
